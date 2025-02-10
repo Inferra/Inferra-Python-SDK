a3d15acef2a21bfdad3c5bcc69c22c025ea79e33
elad <55890652+elad-gr@users.noreply.github.com>
2025-02-11 01:36:05 +0300

    Create __init__.py

---
+++ inferra/api/__init__.py
@@ -0,0 +1,6 @@
+from .chat import ChatAPI
+from .completions import CompletionsAPI
+from .batch import BatchAPI
+from .files import FilesAPI
+
+__all__ = ["ChatAPI", "CompletionsAPI", "BatchAPI", "FilesAPI"]
