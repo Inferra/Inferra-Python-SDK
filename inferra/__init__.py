20693cd5a279d699da377491dab5b7f7a5eafb1c
elad <55890652+elad-gr@users.noreply.github.com>
2025-02-11 01:44:42 +0300

    .

--- inferra/__init__.py
+++
@@ -1,6 +0,0 @@
-from .client import InferraClient
-from .exceptions import InferraAPIError, InferraRateLimitError
-from .version import VERSION
-
-__version__ = VERSION
-__all__ = ["InferraClient", "InferraAPIError", "InferraRateLimitError"]
--- inferra/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from .chat import ChatAPI
-from .completions import CompletionsAPI
-from .batch import BatchAPI
-from .files import FilesAPI
-
-__all__ = ["ChatAPI", "CompletionsAPI", "BatchAPI", "FilesAPI"]
